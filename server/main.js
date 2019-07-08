575ae07457d2af7af112df3ea40358d024ab51c7
Nicholas G Vitovitch <ngvitovitch@gmail.com>
2019-07-08 08:48:38 -0400

    Implement KP's lib1 example using WebXR
    Borrows from immersive-web@github's webxr-samples, specifically
    the 'mirror' and 'fallback' rendering samples, to present a toy
    frag shader rendered to a quad.

---
+++ server/main.js
@@ -0,0 +1,32 @@
+// file: main.js
+// author: Nicholas G Vitovitch <ngv220@nyu.edu>
+//
+// A minimal static webpage demonstrating a shared VR environment
+// using WebXR.
+//
+// NOTE: WebXR !!!REQUIRES!!! a secure context in order to run (i.e. URL must
+// begin with https:// or localhost), otherwise `navigator.xr` is undefined!
+
+
+var https = require('https');
+var fs = require('fs');
+var express = require('express');
+
+var options = {
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert'),
+  requestCert: false,
+  rejectUnauthorized: false
+};
+
+var app = express();
+var frontend = express.static('../frontend');
+
+app.use(frontend);
+
+var port = process.env.PORT || 443;
+var server = https.createServer(options, app);
+
+server.listen(port, function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
