d9a51adfd90b143c1e8d8490cab33e65b1372449
KTRosenberg <ktr254@nyu.edu>
2019-07-20 19:43:09 -0400

    texture example now comments out 2-buffer solution and provides example using an element array buffer and one vertex buffer containing interleaved data

--- frontend/js/worlds/examples/w4/w4_textures.js
+++ frontend/js/worlds/examples/w4/w4_textures.js
@@ -2,8 +2,9 @@
 MR.registerWorld((function() {
     const vert = `#version 300 es
     precision highp float;
-    in vec3 aPos;
-    in vec2 aUV;
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aNor;
+    layout (location = 2) in vec2 aUV;
 
     uniform mat4 uModel;
     uniform mat4 uView;
@@ -12,6 +13,8 @@ MR.registerWorld((function() {
 
     out vec2 vUV;
     out vec2 vUV2;
+
+    out vec3 vNor;
 
     float sin01(float v) {
         return (1.0 + sin(v)) / 2.0;
@@ -41,6 +44,7 @@ MR.registerWorld((function() {
       // Multiply the position by the matrix.
       gl_Position = uProj * uView * uModel * vec4(aPos + vec3(0., 0., -2.0 * (sin01(uTime) + 1.0)), 1.0);
 
+      vNor = aNor;
       // Pass the texcoord to the fragment shader.
       vUV = aUV;
 
@@ -53,8 +57,10 @@ MR.registerWorld((function() {
     precision highp float;
 
     // Passed in from the vertex shader.
+    in vec3 vNor;
     in vec2 vUV;
     in vec2 vUV2;
+
 
     // The texture(s).
     uniform sampler2D uTex0;
@@ -245,15 +251,70 @@ MR.registerWorld((function() {
     ]);
 
 
+    const cubeVertexCount = 24;
+    const cubeIndexCount  = 36;
+    // front, right, back, left, top, bottom
+    const cubeVertexData = new Float32Array([
+        // pos             // normals      // uv coords                                  
+        -1.0,  1.0,  1.0,  0.0, 0.0, 1.0,  0.0, 1.0, // Top Left
+        -1.0, -1.0,  1.0,  0.0, 0.0, 1.0,  0.0, 0.0, // Bottom Left 
+         1.0, -1.0,  1.0,  0.0, 0.0, 1.0,  1.0, 0.0, // Bottom Right
+         1.0,  1.0,  1.0,  0.0, 0.0, 1.0,  1.0, 1.0, // Top Right
+         
+         1.0,  1.0,  1.0,  1.0, 0.0, 0.0,  0.0, 1.0, // Top Left
+         1.0, -1.0,  1.0,  1.0, 0.0, 0.0,  0.0, 0.0, // Bottom Left 
+         1.0, -1.0, -1.0,  1.0, 0.0, 0.0,  1.0, 0.0, // Bottom Right
+         1.0,  1.0, -1.0,  1.0, 0.0, 0.0,  1.0, 1.0, // Top Right
+         
+         1.0,  1.0, -1.0,  0.0, 0.0,-1.0,  0.0, 1.0, // Top Left
+         1.0, -1.0, -1.0,  0.0, 0.0,-1.0,  0.0, 0.0, // Bottom Left 
+        -1.0, -1.0, -1.0,  0.0, 0.0,-1.0,  1.0, 0.0, // Bottom Right
+        -1.0,  1.0, -1.0,  0.0, 0.0,-1.0,  1.0, 1.0, // Top Right
+        
+        -1.0,  1.0, -1.0, -1.0, 0.0, 0.0,  0.0, 1.0, // Top Left
+        -1.0, -1.0, -1.0, -1.0, 0.0, 0.0,  0.0, 0.0, // Bottom Left 
+        -1.0, -1.0,  1.0, -1.0, 0.0, 0.0,  1.0, 0.0, // Bottom Right
+        -1.0,  1.0,  1.0, -1.0, 0.0, 0.0,  1.0, 1.0, // Top Right
+        
+        -1.0,  1.0, -1.0,  0.0, 1.0, 0.0,  0.0, 1.0, // Top Left
+        -1.0,  1.0,  1.0,  0.0, 1.0, 0.0,  0.0, 0.0, // Bottom Left 
+         1.0,  1.0,  1.0,  0.0, 1.0, 0.0,  1.0, 0.0, // Bottom Right
+         1.0,  1.0, -1.0,  0.0, 1.0, 0.0,  1.0, 1.0, // Top Right
+         
+        -1.0, -1.0,  1.0,  0.0,-1.0, 0.0,  0.0, 1.0, // Top Left
+        -1.0, -1.0, -1.0,  0.0,-1.0, 0.0,  0.0, 0.0, // Bottom Left 
+         1.0, -1.0, -1.0,  0.0,-1.0, 0.0,  1.0, 0.0, // Bottom Right
+         1.0, -1.0,  1.0,  0.0,-1.0, 0.0,  1.0, 1.0  // Top Right
+    ]);
+    const cubeIndexData = new Uint16Array([
+        0, 1, 2,
+        2, 3, 0,
+        
+        4, 5, 6,
+        6, 7, 4,
+        
+        8, 9, 10,
+        10, 11, 8,
+        
+        12, 13, 14,
+        14, 15, 12,
+        
+        16, 17, 18,
+        18, 19, 16,
+        
+        20, 21, 22,
+        22, 23, 20
+    ]);
+
+
+
     function setup(state, myWorld, session) {
-        // load initial images, then do gl setup (alternative is to do setup with placeholder
-        // texture or to allow user to set a new draw function programmatically
+        // load initial images, then continue setup
         loadImages([
             window.location + "js/worlds/examples/w4/resources/textures/brick.png",
             window.location + "js/worlds/examples/w4/resources/textures/polkadots_transparent.png",    
         ],
         (images) => {
-        myWorld.start();
         
         state.attribData  = {};
         state.uniformData = {};
@@ -267,7 +328,9 @@ MR.registerWorld((function() {
         // save all attribute and uniform locations
 
         state.attribData.posAttribLoc = gl.getAttribLocation(state.program, "aPos");
+        state.attribData.norAttribLoc = gl.getAttribLocation(state.program, "aNor");
         state.attribData.uvAttribLoc  = gl.getAttribLocation(state.program, "aUV");
+        console.log(state.attribData.posAttribLoc, state.attribData.norAttribLoc, state.attribData.uvAttribLoc);
 
         // note: could also use a uniform buffer instead of individual uniforms
         // to share uniforms across shader programs
@@ -285,100 +348,169 @@ MR.registerWorld((function() {
         state.vao = gl.createVertexArray();
         gl.bindVertexArray(state.vao);
         
+        // create buffer for attributes
+        // Version 1: (RECOMMENDED)
+        {
+            // create buffer for vertex attribute data
+            state.vertexBuf = gl.createBuffer();
+            // set this to be the buffer we're currently looking at
+            gl.bindBuffer(gl.ARRAY_BUFFER, state.vertexBuf);
+            // upload data to buffer
+            gl.bufferData(gl.ARRAY_BUFFER, cubeVertexData, gl.STATIC_DRAW, 0);
+
+            // create buffer for indexing into vertex buffer
+            state.elementBuf = gl.createBuffer();
+            // set this to be the buffer we're currently looking at
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, state.elementBuf);
+            // upload data to buffer
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, cubeIndexData, gl.STATIC_DRAW, 0);
+
+            // specify the attributes
+            // TODO(KTR): possible helper function call
+            //function setAttribPointers(state.attribData.posAttribLoc, size, type, normalize, stride, offset, bytesPerElement);
+            
+            // position
+            {
+                const size = 3;                                    // 3 components per iteration
+                const type = gl.FLOAT;                             // the data is 32-bit floats
+                const normalize = false;                           // don't normalize data
+                const stride = Float32Array.BYTES_PER_ELEMENT * 8; // move forward (size * sizeof(type)) ... using 3 for ps, 3 for normal, 2 for uv (8)
+                const offset = Float32Array.BYTES_PER_ELEMENT * 0;
+                // set how the data is accessed in the buffer
+                gl.vertexAttribPointer( 
+                    state.attribData.posAttribLoc, size, type, normalize, stride, offset
+                );
+                gl.enableVertexAttribArray(state.attribData.posAttribLoc); // enable the attribute
+            }
+            // normals
+            {
+                const size = 3;                                    // 3 components per iteration
+                const type = gl.FLOAT;                             // the data is 32-bit floats
+                const normalize = false;                           // don't normalize data
+                const stride = Float32Array.BYTES_PER_ELEMENT * 8; // move forward (size * sizeof(type)) ... using 3 for ps, 3 for normal, 2 for uv (8)
+                const offset = Float32Array.BYTES_PER_ELEMENT * 3;
+                // set how the data is accessed in the buffer
+                gl.vertexAttribPointer( 
+                    state.attribData.norAttribLoc, size, type, normalize, stride, offset
+                );
+                gl.enableVertexAttribArray(state.attribData.norAttribLoc); // enable the attribute
+            }
+            // uv coords
+            {
+                const size = 2;                                    // 3 components per iteration
+                const type = gl.FLOAT;                             // the data is 32-bit floats
+                const normalize = false;                           // don't normalize data
+                const stride = Float32Array.BYTES_PER_ELEMENT * 8; // move forward (size * sizeof(type)) ... using 3 for ps, 3 for normal, 2 for uv (8)
+                const offset = Float32Array.BYTES_PER_ELEMENT * 6;
+                // set how the data is accessed in the buffer
+                gl.vertexAttribPointer( 
+                    state.attribData.uvAttribLoc, size, type, normalize, stride, offset
+                );
+                gl.enableVertexAttribArray(state.attribData.uvAttribLoc); // enable the attribute
+            }
+        }
+        // Version 2: (OLD)
         // This version uses 2 buffers to store position and UV/texture coord
         // attributes separately, but usually we may want to use a single
         // buffer with interleaved data
-        {   
-            // position buffer
-            state.posBuffer = gl.createBuffer();
-            // set this to be the buffer we're currently looking at
-            gl.bindBuffer(gl.ARRAY_BUFFER, state.posBuffer);
-            // upload data to buffer
-            gl.bufferData(gl.ARRAY_BUFFER, geometryPosData, gl.STATIC_DRAW);
+        // {
+        //     {   
+        //         // position buffer
+        //         state.posBuffer = gl.createBuffer();
+        //         // set this to be the buffer we're currently looking at
+        //         gl.bindBuffer(gl.ARRAY_BUFFER, state.posBuffer);
+        //         // upload data to buffer
+        //         gl.bufferData(gl.ARRAY_BUFFER, geometryPosData, gl.STATIC_DRAW);
 
-            const size = 3;          // 3 components per iteration
-            const type = gl.FLOAT;   // the data is 32-bit floats
-            const normalize = false; // don't normalize data
-            const stride = 0;        // 0 = move forward (size * sizeof(type))
-            const offset = 0;        // start at beginning of the buffer
-            // set how the data is accessed in the buffer
-            gl.vertexAttribPointer( 
-                state.attribData.posAttribLoc, size, type, normalize, stride, offset
-            );
-            gl.enableVertexAttribArray(state.attribData.posAttribLoc); // enable the attribute
-        }
+        //         const size = 3;          // 3 components per iteration
+        //         const type = gl.FLOAT;   // the data is 32-bit floats
+        //         const normalize = false; // don't normalize data
+        //         const stride = 0;        // 0 = move forward (size * sizeof(type))
+        //         const offset = 0;        // start at beginning of the buffer
+        //         // set how the data is accessed in the buffer
+        //         gl.vertexAttribPointer( 
+        //             state.attribData.posAttribLoc, size, type, normalize, stride, offset
+        //         );
+        //         gl.enableVertexAttribArray(state.attribData.posAttribLoc); // enable the attribute
+        //     }
 
+        //     {
+        //         // uv buffer
+        //         state.uvBuffer = gl.createBuffer();
+        //         // set this to be the buffer we're currently looking at
+        //         gl.bindBuffer(gl.ARRAY_BUFFER, state.uvBuffer);
+        //         // upload data to buffer
+        //         gl.bufferData(gl.ARRAY_BUFFER, geometryUVData, gl.STATIC_DRAW); 
+
+        //         const size = 2;
+        //         const type = gl.FLOAT;
+        //         const normalize = true;
+        //         const stride = 0;
+        //         const offset = 0;
+        //         gl.vertexAttribPointer(
+        //             state.attribData.uvAttribLoc, size, type, normalize, stride, offset
+        //         );
+        //         gl.enableVertexAttribArray(state.attribData.uvAttribLoc); // enable the attribute
+        //     }
+        // }
+
+        // load textures,
+        // alternatively could combine the images into one and use an atlas
         {
-            // uv buffer
-            state.uvBuffer = gl.createBuffer();
-            // set this to be the buffer we're currently looking at
-            gl.bindBuffer(gl.ARRAY_BUFFER, state.uvBuffer);
-            // upload data to buffer
-            gl.bufferData(gl.ARRAY_BUFFER, geometryUVData, gl.STATIC_DRAW); 
+            state.textureData.textures = [];
 
-            const size = 2;
-            const type = gl.FLOAT;
-            const normalize = true;
-            const stride = 0;
-            const offset = 0;
-            gl.vertexAttribPointer(
-                state.attribData.uvAttribLoc, size, type, normalize, stride, offset
-            );
-            gl.enableVertexAttribArray(state.attribData.uvAttribLoc); // enable the attribute
+            const texArr = state.textureData.textures;
+            for (let i = 0; i < state.images.length; i += 1) {
+                const tex = gl.createTexture();
+                texArr.push(tex);
 
-            // alternatively could combine the images into one and use an atlas
-            {
-                state.textureData.textures = [];
+                // look at texture unit 0 + i
+                // (yes, you can just increment TEXTURE0, which is constant number, by an offset. 
+                // The TEXTURE<N>s are all assigned contiguous valures
+                gl.activeTexture(gl.TEXTURE0 + i);
+                // bind texture to the unit
+                gl.bindTexture(gl.TEXTURE_2D, tex); 
 
-                const texArr = state.textureData.textures;
-                for (let i = 0; i < state.images.length; i += 1) {
-                    const tex = gl.createTexture();
-                    texArr.push(tex);
+                // note, there are other options too! Not every texture neats to have the same
+                // parameters set -- you just need to do more than loop over each texture the same way
+                //
+                // gl.REPEAT: The default behavior for textures. Repeats the texture image.
+                // gl.MIRRORED_REPEAT: Same as REPEAT but mirrors the image with each repeat.
+                // gl.CLAMP_TO_EDGE: Clamps the coordinates between 0 and 1. The result is that higher coordinates become clamped to the edge, resulting in a stretched edge pattern.
+                // gl.CLAMP_TO_BORDER: Coordinates outside the range are now given a user-specified border color.
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
-                    // look at texture unit 0 + i
-                    // (yes, you can just increment TEXTURE0, which is constant number, by an offset. 
-                    // The TEXTURE<N>s are all assigned contiguous valures
-                    gl.activeTexture(gl.TEXTURE0 + i);
-                    // bind texture to the unit
-                    gl.bindTexture(gl.TEXTURE_2D, tex); 
-
-                    // note, there are other options too! Not every texture neats to have the same
-                    // parameters set -- you just need to do more than loop over each texture the same way
-                    //
-                    // gl.REPEAT: The default behavior for textures. Repeats the texture image.
-                    // gl.MIRRORED_REPEAT: Same as REPEAT but mirrors the image with each repeat.
-                    // gl.CLAMP_TO_EDGE: Clamps the coordinates between 0 and 1. The result is that higher coordinates become clamped to the edge, resulting in a stretched edge pattern.
-                    // gl.CLAMP_TO_BORDER: Coordinates outside the range are now given a user-specified border color.
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-                    // upload the texture to the current texture unit
-                    const mipLevel = 0;                      // largest mip is 0
-                    const internalFormat = gl.RGBA;          // desired texture format
-                    const srcFormat      = gl.RGBA;          // format of data
-                    const srcType        = gl.UNSIGNED_BYTE; // type of data
-                    gl.texImage2D(
-                        gl.TEXTURE_2D,
-                        mipLevel,
-                        internalFormat,
-                        srcFormat,
-                        srcType,
-                        state.images[i]
-                    );
-                    // generate mipmaps for the texture 
-                    // (note: mipmapping is not required for all
-                    // systems. e.g. 2D sprite games don't always need this)
-                    gl.generateMipmap(gl.TEXTURE_2D);
-                }
-                // discard images unless we actually need them later (in this example, we don't)
-                state.images = undefined;
+                // upload the texture to the current texture unit
+                const mipLevel = 0;                      // largest mip is 0
+                const internalFormat = gl.RGBA;          // desired texture format
+                const srcFormat      = gl.RGBA;          // format of data
+                const srcType        = gl.UNSIGNED_BYTE; // type of data
+                gl.texImage2D(
+                    gl.TEXTURE_2D,
+                    mipLevel,
+                    internalFormat,
+                    srcFormat,
+                    srcType,
+                    state.images[i]
+                );
+                // generate mipmaps for the texture 
+                // (note: mipmapping is not required for all
+                // systems. e.g. 2D sprite games don't always need this)
+                gl.generateMipmap(gl.TEXTURE_2D);
             }
-
-            gl.enable(gl.BLEND);
-            gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+            // discard images unless we actually need them later (in this example, we don't)
+            state.images = undefined;
         }
+
+        gl.enable(gl.BLEND);
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+        // don't forget to start the world!
+        myWorld.start();
+        
         });
     }
 
@@ -407,7 +539,7 @@ MR.registerWorld((function() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         gl.enable(gl.DEPTH_TEST);
-        gl.enable(gl.CULL_FACE);
+        //gl.enable(gl.CULL_FACE);
         
             gl.bindVertexArray(state.vao);
 
@@ -430,8 +562,10 @@ MR.registerWorld((function() {
 
         const primitive = gl.TRIANGLES;
         const offset    = 0;
-        const count     = 6 * 6;
-        gl.drawArrays(primitive, offset, count);
+        const count     = cubeIndexCount;
+        // Note: we could choose to optimize memory use further by using UNSIGNED_BYTE since we have so few indices (< 255),
+        // but most of the time you would use UNSIGNED_SHORT or UNSIGNED_INT
+        gl.drawElements(primitive, cubeIndexCount, gl.UNSIGNED_SHORT, offset);
     }
 
 
