2be3932cf9f56832ccb073a1358076d81f1f28dc
Zhenyi He <snowymohehe@gmail.com>
2020-01-01 08:28:16 -0500

    :recycle: moving main.js from deep inside of system folder to root folder, and apply changes to init and run

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'Front End',
-    script: './system/server/main.js',
+    script: './main.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     // args: 'one two',
--- init.bat
+++ init.bat
@@ -1,4 +1,4 @@
 git submodule init
 git submodule update --recursive
 
-npm install --cwd ".\system\server" --prefix ".\system\server" && npm install --cwd ".\webxr-server" --prefix ".\webxr-server" && npm install -g pm2
+npm install && npm install --cwd ".\webxr-server" --prefix ".\webxr-server" && npm install -g pm2
