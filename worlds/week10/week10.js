9c60ed5598adf91ce3bcbeb327b3d12df362044d
Zhenyi He <snowymohehe@gmail.com>
2020-01-07 17:29:31 -0500

    :beer: revert the week10.js in two folders back to the status before large refactoring.

--- worlds/week10/week10.js
+++ worlds/week10/week10.js
@@ -2,6 +2,7 @@
 
 import {Lock} from "../../lib/core/lock.js";
 
+
 /*--------------------------------------------------------------------------------
 
 The proportions below just happen to match the dimensions of my physical space
@@ -12,20 +13,6 @@ Note that I measured everything in inches, and then converted to units of meters
 
 --------------------------------------------------------------------------------*/
 
-const inchesToMeters = inches => inches * 0.0254;
-const metersToInches = meters => meters / 0.0254;
-
-const EYE_HEIGHT       = inchesToMeters( 69);
-const HALL_LENGTH      = inchesToMeters(306);
-const HALL_WIDTH       = inchesToMeters(215);
-const RING_RADIUS      = 0.0425;
-const STOOL_HEIGHT     = inchesToMeters( 18);
-const STOOL_RADIUS     = inchesToMeters( 10);
-const TABLE_DEPTH      = inchesToMeters( 30);
-const TABLE_HEIGHT     = inchesToMeters( 29);
-const TABLE_WIDTH      = inchesToMeters( 60);
-const TABLE_THICKNESS  = inchesToMeters( 11/8);
-const LEG_THICKNESS    = inchesToMeters(  2.5);
 
 const ENABLE_BRIGHTNESS_CHECK = false;
 
