d1cefa2560d9568469c8e94eaf0b3917ce5d28c0
Ben Ahlbrand <bmahlbrand@gmail.com>
2020-01-11 03:20:40 -0500

    pm2 fix (#69)

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
 REM node server/main.js %*
-REM stop
-pm2 start server/ecosystem.config.js && pm2 logs
+pm2 startOrReload server/ecosystem.config.js && pm2 logs
 REM open browser and go to localhost:3000
